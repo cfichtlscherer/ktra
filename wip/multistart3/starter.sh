44861529671382fa8f9adc482cfd3635ecb51195
cpf <chris1218@gmx.de>
2021-04-27 23:14:21 +0200

    type in function name in the bash  / remove trailing white spaces

--- wip/multistart3/starter.sh
+++ wip/multistart3/starter.sh
@@ -32,31 +32,30 @@ cp -r block 20-0.02-0.03-3
 cp -r block 20-0.03-0.03-3
 cp -r block 20-0.04-0.03-3
 
-
-cd 20-0.01-0.01-2 && python3 douglas_rachedord_multi.py &\ 
-cd 20-0.02-0.01-2 && python3 douglas_rachedord_multi.py &\
-cd 20-0.03-0.01-2 && python3 douglas_rachedord_multi.py &\
-cd 20-0.04-0.01-2 && python3 douglas_rachedord_multi.py &\
-cd 20-0.01-0.01-3 && python3 douglas_rachedord_multi.py &\
-cd 20-0.02-0.01-3 && python3 douglas_rachedord_multi.py &\
-cd 20-0.03-0.01-3 && python3 douglas_rachedord_multi.py &\
-cd 20-0.04-0.01-3 && python3 douglas_rachedord_multi.py &\ 
-cd 20-0.01-0.02-2 && python3 douglas_rachedord_multi.py &\ 
-cd 20-0.02-0.02-2 && python3 douglas_rachedord_multi.py &\ 
-cd 20-0.03-0.02-2 && python3 douglas_rachedord_multi.py &\ 
-cd 20-0.04-0.02-2 && python3 douglas_rachedord_multi.py &\ 
-cd 20-0.01-0.02-3 && python3 douglas_rachedord_multi.py &\ 
-cd 20-0.02-0.02-3 && python3 douglas_rachedord_multi.py &\
-cd 20-0.03-0.02-3 && python3 douglas_rachedord_multi.py &\
-cd 20-0.04-0.02-3 && python3 douglas_rachedord_multi.py &\
-cd 20-0.01-0.03-2 && python3 douglas_rachedord_multi.py &\
-cd 20-0.02-0.03-2 && python3 douglas_rachedord_multi.py &\
-cd 20-0.03-0.03-2 && python3 douglas_rachedord_multi.py &\ 
-cd 20-0.04-0.03-2 && python3 douglas_rachedord_multi.py &\ 
-cd 20-0.01-0.03-3 && python3 douglas_rachedord_multi.py &\
-cd 20-0.02-0.03-3 && python3 douglas_rachedord_multi.py &\
-cd 20-0.03-0.03-3 && python3 douglas_rachedord_multi.py &\
-cd 20-0.04-0.03-3 && python3 douglas_rachedord_multi.py 
+cd 20-0.01-0.01-2 && python3 douglas_rachford_multi.py &\
+cd 20-0.02-0.01-2 && python3 douglas_rachford_multi.py &\
+cd 20-0.03-0.01-2 && python3 douglas_rachford_multi.py &\
+cd 20-0.04-0.01-2 && python3 douglas_rachford_multi.py &\
+cd 20-0.01-0.01-3 && python3 douglas_rachford_multi.py &\
+cd 20-0.02-0.01-3 && python3 douglas_rachford_multi.py &\
+cd 20-0.03-0.01-3 && python3 douglas_rachford_multi.py &\
+cd 20-0.04-0.01-3 && python3 douglas_rachford_multi.py &\
+cd 20-0.01-0.02-2 && python3 douglas_rachford_multi.py &\
+cd 20-0.02-0.02-2 && python3 douglas_rachford_multi.py &\
+cd 20-0.03-0.02-2 && python3 douglas_rachford_multi.py &\
+cd 20-0.04-0.02-2 && python3 douglas_rachford_multi.py &\
+cd 20-0.01-0.02-3 && python3 douglas_rachford_multi.py &\
+cd 20-0.02-0.02-3 && python3 douglas_rachford_multi.py &\
+cd 20-0.03-0.02-3 && python3 douglas_rachford_multi.py &\
+cd 20-0.04-0.02-3 && python3 douglas_rachford_multi.py &\
+cd 20-0.01-0.03-2 && python3 douglas_rachford_multi.py &\
+cd 20-0.02-0.03-2 && python3 douglas_rachford_multi.py &\
+cd 20-0.03-0.03-2 && python3 douglas_rachford_multi.py &\
+cd 20-0.04-0.03-2 && python3 douglas_rachford_multi.py &\
+cd 20-0.01-0.03-3 && python3 douglas_rachford_multi.py &\
+cd 20-0.02-0.03-3 && python3 douglas_rachford_multi.py &\
+cd 20-0.03-0.03-3 && python3 douglas_rachford_multi.py &\
+cd 20-0.04-0.03-3 && python3 douglas_rachford_multi.py
 
 
 
