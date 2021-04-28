d0ab25bb9b33a9aaab7e5072541d70f20cfc5f80
cpf <chris1218@gmx.de>
2021-04-28 09:10:03 +0200

    minor bug

--- wip/ssim_multi1/starter.sh
+++ wip/ssim_multi1/starter.sh
@@ -38,36 +38,36 @@ cp -r block 20-0.040-0.03-3
 cp -r block 20-0.045-0.03-3
 cp -r block 20-0.050-0.03-3
 
-cd block 20-0.005-0.01-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.010-0.01-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.015-0.01-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.020-0.01-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.025-0.01-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.030-0.01-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.035-0.01-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.040-0.01-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.045-0.01-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.050-0.01-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.005-0.01-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.010-0.01-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.015-0.01-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.020-0.01-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.025-0.01-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.030-0.01-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.035-0.01-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.040-0.01-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.045-0.01-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.050-0.01-3  && python3 douglas_rachford_multi.py &\
 
-cd block 20-0.005-0.02-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.010-0.02-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.015-0.02-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.020-0.02-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.025-0.02-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.030-0.02-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.035-0.02-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.040-0.02-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.045-0.02-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.050-0.02-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.005-0.02-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.010-0.02-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.015-0.02-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.020-0.02-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.025-0.02-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.030-0.02-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.035-0.02-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.040-0.02-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.045-0.02-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.050-0.02-3  && python3 douglas_rachford_multi.py &\
 
-cd block 20-0.005-0.03-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.010-0.03-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.015-0.03-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.020-0.03-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.025-0.03-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.030-0.03-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.035-0.03-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.040-0.03-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.045-0.03-3  && python3 douglas_rachford_multi.py &\
-cd block 20-0.050-0.03-3  && python3 douglas_rachford_multi.py
+cd 20-0.005-0.03-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.010-0.03-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.015-0.03-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.020-0.03-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.025-0.03-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.030-0.03-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.035-0.03-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.040-0.03-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.045-0.03-3  && python3 douglas_rachford_multi.py &\
+cd 20-0.050-0.03-3  && python3 douglas_rachford_multi.py
 
